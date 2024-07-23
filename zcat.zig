3d3f2cf9346664b9eae563607b68e1d0e7d95b3c
nishan8583 <nishanmaharjan17@gmail.com>
2024-07-23 07:57:22 -0700

    reading from a file is working

--- zcat.zig
+++ zcat.zig
@@ -1,15 +1,61 @@
 const std = @import("std");
 const fs = std.fs;
 const Allocator = std.mem.Allocator; // used just as a type, in the read_whole_file function
-const expect = std.testing.expect;                                     
+const expect = std.testing.expect;
+
+pub fn read_file_line_by_line(file_path: []const u8) !void {
+    var allocator = std.heap.page_allocator;
+    var buffer = try allocator.alloc(u8, 512);
+    //    _ = buffer;  // for some reason compiler treats this as never mutated, even if we
+    //    write to this buffer
+    defer allocator.free(buffer);
+
+    var file: fs.File = fs.openFileAbsolute(file_path, .{ .mode = fs.File.OpenMode.read_only }) catch |err| {
+        return err;
+    };
+
+    defer file.close();
+
+    var seek: usize = 0;
+    while (true) {
+        const bytes_read = try file.read(buffer);
+
+        // EOF is not an error indication
+        // when EOF is reached bytes_read is smaller than buffer.len
+        if (bytes_read == 0) {
+            break;
+        }
+        seek += bytes_read;
+        file.seekTo(seek) catch |err| {
+            std.debug.print("could not seek to position {d}\n", .{seek});
+            std.debug.print("{s}", .{buffer[0..bytes_read]});
+            return err;
+        };
+
+        std.debug.print("{s}\n", .{buffer[0..bytes_read]});
+        for (buffer[0..bytes_read]) |byte| {
+            std.debug.print("{d} ", .{byte});
+        }
+    }
+}
 
 pub fn main() !void {
 
     // we need to create a custom allocator, any standard library function will not allocate memory on its own
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+    //var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    //const allocator = gpa.allocator();
+    //var allocator = std.heap.page_allocator;
+    //var file_path = allocator.alloc(u8, 1024);
+    //const file_path = "C:\\D\\projects\\Zig Projects\\example.txt";
 
-    try read_whole_file(allocator, "example.txt");
+    const path = "C:\\D\\projects\\Zig Projects\\example.txt";
+    //var mutable_path: [38]u8 = undefined;
+    //std.mem.copy(u8, &mutable_path, path_str);
+
+    // Cast the const array to a mutable slice
+    try read_file_line_by_line(path);
+
+    //try read_whole_file(allocator, "example.txt");
 }
 
 // read_whole_file takes an allocator, path its a string, returns error if any
@@ -19,21 +65,4 @@ pub fn read_whole_file(allocator: Allocator, path: []const u8) !void {
 
     // output content to the screen
     try std.io.getStdOut().writer().writeAll(data);
-}
-
-
-
-
-pub fn read_file(file_path: []const u8) {
-    var file = try fs.openFileAbsolute(file_path,.{ .read =true });
-    defer file.close();
-
-    var buffer: [100]u8 = undefined;
-    const bytes_read = try file.readAll();
-
-    return bufffer;
-}
-
-test "readFile" {
-    expect(read_file("C:\\D\\file.txt") =="Hello World");
 }
--- zcat_test.zig
+++ zcat_test.zig
@@ -58,3 +58,63 @@ test "create_file read_file" {
     //       std.debug.print("equal", .{});
     //  }
 }
+
+pub fn read_file_line_by_line(file_path: []u8, buffer: []u8, temp_buffer: []u8) !void {
+    var file: fs.File = fs.openFileAbsolute(file_path, .{ .mode: fs.File.OpenMode.read_only }) catch |err| {
+        return err;
+    }
+    defer file.close();
+    
+    var seek = 0;
+    while (true) {
+        const bytes_read: u8 = try file.read(temp_buffer);
+
+        // EOF is not an error indication
+        // when EOF is reached bytes_read is smaller than buffer.len
+        if bytes_read < temp_buffer.len() {
+            buffer[seek..]
+            break;
+        }
+        seek += bytes_read;
+        file.seekTo(seek) catch |err| {
+            std.debug.print("could not seek to position {d}\n",.{seek});
+            return err;
+        };
+
+    }
+
+}
+
+test "read line by line" {
+    var allocator = std.heap.page_allocator;
+    const file_content = "x";
+    const file_name = "read_line_by_line_test.txt";
+
+    const file = try fs.cwd().createFile(
+        file_name,
+        .{ .read = false },
+    );
+    defer fs.cwd().deleteFile(file_name);
+
+    for (0..100000) | _ | {
+     
+        // .writeAll() ensures entire buffer is written in one call
+        try file.writeAll(file_content,file_content);
+
+    }
+
+    file.Close();
+
+    var buffer =try allocator.alloc(1000000);
+    var temp_buffer = try allocator.alloc(100);
+    defer allocator.free(temp_buffer);
+    defer allocator.free(buffer);
+
+
+    try read_file_line_by_line(file_name,buffer,temp_buffer);
+    try testing.expect.equal(100000 == buffer.len);
+    for (buffer, 0..) | char,_ | {
+        testing.expect.equal("x" == char);
+    }
+
+}
