c3913ba5ac1b4b2b16bb7bb9d5ab2bf640ae58a5
nishan8583 <nishanmaharjan17@gmail.com>
2024-07-28 09:14:20 -0700

    first zcat is working, absolute path is good, need to work on relative file path, also created directory structure, and created readme

--- toy_projects/zcat/zcat.zig
+++ toy_projects/zcat/zcat.zig
@@ -9,11 +9,18 @@ pub fn read_file_line_by_line(file_path: []const u8) !void {
     //    _ = buffer;  // for some reason compiler treats this as never mutated, even if we
     //    write to this buffer
     defer allocator.free(buffer);
+    var file: fs.File = undefined;
 
-    var file: fs.File = fs.openFileAbsolute(file_path, .{ .mode = fs.File.OpenMode.read_only }) catch |err| {
-        return err;
-    };
-
+    if (fs.path.isAbsolute(file_path)) {
+        file = fs.openFileAbsolute(file_path, .{ .mode = fs.File.OpenMode.read_only }) catch |err| {
+            return err;
+        };
+    } else {
+        const dir = fs.cwd();
+        file = dir.openFile(file_path, .{ .mode = fs.File.OpenMode.read_only }) catch |err| {
+            return err;
+        };
+    }
     defer file.close();
 
     var seek: usize = 0;
@@ -39,6 +46,24 @@ pub fn read_file_line_by_line(file_path: []const u8) !void {
     }
 }
 
+pub fn read_file() !void {
+    const allocator = std.heap.page_allocator;
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+    _ = args.next(); // ignore itself
+    while (true) {
+        const v = args.next(); // args.next() returns ?([:0]const u8), ? means it returns either null or the other value, the we handle this optional type is in the below if condition
+        if (v) |result| {
+            std.debug.print("The arguement {s}", .{result});
+            read_file_line_by_line(result) catch |err| {
+                return err;
+            };
+        } else {
+            break;
+        }
+    }
+}
+
 pub fn main() !void {
 
     // we need to create a custom allocator, any standard library function will not allocate memory on its own
@@ -48,18 +73,21 @@ pub fn main() !void {
     //var file_path = allocator.alloc(u8, 1024);
     //const file_path = "C:\\D\\projects\\Zig Projects\\example.txt";
 
-    const path = "C:\\D\\projects\\Zig Projects\\example.txt";
+    //const path = "C:\\D\\projects\\Zig Projects\\example.txt";
     //var mutable_path: [38]u8 = undefined;
     //std.mem.copy(u8, &mutable_path, path_str);
 
     // Cast the const array to a mutable slice
-    try read_file_line_by_line(path);
+    //try read_file_line_by_line(path);
 
     //try read_whole_file(allocator, "example.txt");
+    //
+    try read_file();
 }
 
 // read_whole_file takes an allocator, path its a string, returns error if any
-pub fn read_whole_file(allocator: Allocator, path: []const u8) !void {
+pub fn read_whole_file(path: []const u8) !void {
+    const allocator = std.heap.page_allocator;
     const data = try fs.cwd().readFileAlloc(allocator, path, 1000);
     defer allocator.free(data); // defer called when returning from the function
 
