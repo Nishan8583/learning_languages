08a0d431429513fc129b82df338519f0553f1ede
nishan8583 <nishanmaharjan17@gmail.com>
2025-02-23 18:37:05 -0800

    file creation and deletion was successfully, need some sort of cli to make things more clear

--- c/file_system/fs.h
+++ c/file_system/fs.h
@@ -9,6 +9,7 @@
 
 #define MAX_FILE_ENTRIES 10000
 #define MAX_FILE_NAME 100
+#define MAX_FILE_SIZE 4096
 #define FILE_SYSTEM_PATH "fs.bin"
 #define TEST_FILE_SYSTEM_PATH "fs_test.bin"
 
@@ -25,6 +26,13 @@
 #define SUCCESS_CREATE_FILE 0
 #define FAILURE_CREATE_FILE 1
 
+#define REMOVE_FILE_SUCCESS 0
+#define REMOVE_FILE_ERROR 1
+
+#define WRITE_FILE_FAILURE 0
+
+#define READ_FILE_FAILURE 0
+
 #define true 1
 #define false 0
 
@@ -35,9 +43,17 @@ typedef struct {
   int block_index;
 } FILE_ENTRY;
 
+typedef struct {
+  char *blob[MAX_FILE_ENTRIES]; // so create a array of pointer, of size
+                                // MAX_FILE_ENTRIES, and now for each file
+                                // entries when we want to write we have to
+                                // allocate memory
+} STORAGE_BLOB;
+
 // FILE_SYSTEM struct contains array of FILE_ENTRY
 typedef struct {
   FILE_ENTRY files[MAX_FILE_ENTRIES];
+  STORAGE_BLOB blob_storage;
 } FILE_SYSTEM;
 
 // save_fs takes pointer to FILE_SYSTEM and saves to disk as pointed by PATH
@@ -87,10 +103,61 @@ int create_file(FILE_SYSTEM *fs, char *filename) {
                               // done, so we are copying, also we do not want
                               // buffere overflow in user controlled value, so
                               // we use strncpy
+      // todo
+      // Update block index from storage block
+      char *file_content = calloc(MAX_FILE_SIZE, sizeof(char));
+      fs->blob_storage.blob[i] = file_content;
+      fs->files[i].block_index = i;
+      fs->files[i].size = MAX_FILE_SIZE;
       return SUCCESS_CREATE_FILE;
     }
   }
   return FAILURE_CREATE_FILE;
+}
+
+int remove_file(FILE_SYSTEM *fs, char *filename) {
+  for (int i = 0; i < MAX_FILE_ENTRIES; i++) {
+    if (strcmp(fs->files[i].file_name, filename) == 0) {
+      // found
+      // remove filename
+      memset(fs->files[i].file_name, 0, MAX_FILE_NAME);
+      // memset content to 0
+      memset(fs->blob_storage.blob[i], 0, 4096);
+      // free the allocated memory
+      free(fs->blob_storage.blob[i]);
+
+      fs->blob_storage.blob[i] = NULL;
+      // todo update storage block and block index
+      return REMOVE_FILE_SUCCESS;
+    }
+  }
+  return REMOVE_FILE_ERROR;
+}
+
+int write_to_file(FILE_SYSTEM *fs, char *file_name, char *file_content) {
+  for (int i = 0; i < MAX_FILE_ENTRIES; i++) {
+    // found file
+    if (strcmp(fs->files[i].file_name, file_name) == 0) {
+      strncpy(fs->blob_storage.blob[i], file_content, MAX_FILE_SIZE);
+      if (strlen(file_content) < MAX_FILE_SIZE) {
+        return strlen(file_content);
+      } else {
+        return MAX_FILE_SIZE;
+      }
+    }
+  }
+  return WRITE_FILE_FAILURE;
+}
+
+char *read_from_file(FILE_SYSTEM *fs, char *file_name) {
+  for (int i = 0; i < MAX_FILE_ENTRIES; i++) {
+    // found file
+    if (strcmp(fs->files[i].file_name, file_name) == 0) {
+      return fs->blob_storage.blob[i];
+    }
+  }
+
+  return NULL;
 }
 
 // file_exists checks if the file *filename exists in fs.files
@@ -106,6 +173,8 @@ int file_exists(FILE_SYSTEM *fs, char *filename) {
 // test_read_and_load_fs runs basic sanity test check
 void test_read_and_load_fs(int DEBUG) {
 
+  char file_name[] = "test.txt";
+  char file_content[] = "This is a dummy file content";
   // Act - 1: Create a file system
   FILE_SYSTEM *fs = calloc(1, sizeof(FILE_SYSTEM));
   if (fs == NULL) {
@@ -119,7 +188,6 @@ void test_read_and_load_fs(int DEBUG) {
   }
 
   // Act - 2: Create a file in that file system
-  char file_name[] = "test.txt";
   if (create_file(fs, file_name) != SUCCESS_CREATE_FILE) {
     perror("test_read_and_load_fs(): unable to create_file\n");
     exit(-1);
@@ -163,9 +231,60 @@ void test_read_and_load_fs(int DEBUG) {
     exit(-1);
   }
 
+  // Act - 7: Write to file
+  if (DEBUG) {
+    printf("test_read_and_load_fs(): attemtpting to write to a file\n");
+  }
+  if (write_to_file(fs, file_name, file_content) == WRITE_FILE_FAILURE) {
+    perror("test_read_and_load_fs(): writing to file failure");
+    exit(-7);
+  }
+  if (DEBUG) {
+    printf("Written to file successfully\n");
+  }
+  // Act - 8: Check if file content matched
+  char *content = read_from_file(fs, file_name);
+  if (content == NULL) {
+    perror("test_read_and_load_fs(): read_from file returned NULL pointer");
+    exit(-8);
+  }
+
+  if (strcmp(content, file_content) != 0) {
+    perror("test_read_and_load_fs(): file contents are not same");
+    exit(-1);
+  }
+  if (DEBUG) {
+    printf("File content %s \n", content);
+  }
+
+  // ACt - 9: Remove the file
+  if (remove_file(fs, file_name) != REMOVE_FILE_SUCCESS) {
+    perror("test_read_and_load_fs(): could not remove file\n");
+    exit(-7);
+  }
+  if (DEBUG) {
+    printf("file %s was removed successfully\n", file_name);
+  }
+
+  // ACt - 10: Check if removed file exist
+  if (file_exists(fs, file_name)) {
+    perror("test_read_and_load_fs(): file should not exist after removal but "
+           "does\n");
+    exit(-8);
+  }
+  if (DEBUG) {
+    printf("test_read_and_load_fs(): file remove test case passed\n");
+  }
   printf("test_read_and_load_fs(): SUCCESS\n");
 
-  // ACt - 7: Cleanup
+  // check if file content still exists
+  if (read_from_file(fs, file_name) != NULL) {
+    perror("test_read_and_load_fs(): file content should now be null\n");
+  }
+  if (DEBUG) {
+    printf("test_read_and_load_fs(): file content was cleaned successfully\n");
+  }
+  // ACt - 11: Cleanup
   if (remove(TEST_FILE_SYSTEM_PATH) != 0) {
     perror("test_read_and_load_fs(): error during cleanup");
   }
--- c/file_system/main.c
+++ c/file_system/main.c
@@ -1,12 +1,8 @@
 #include "fs.h"
-#include <stdio.h>
 
 int main(int argc, char **argv) {
   if (argc > 1) {
-    printf("|%s|\n", argv[1]);
     if (strcmp(argv[1], "--test") == 0) {
-      printf("test found\n");
-      printf("|%s|\n", argv[2]);
       if (argc < 3) {
         test_read_and_load_fs(NON_DEBUG_MODE);
       } else if (strcmp(argv[2], "--debug") == 0) {
