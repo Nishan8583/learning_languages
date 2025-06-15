d1f1f83fe21fbc7e09c43b9f1da39b43cf703bcc
Nishan8583 <nishanmaharjan17@gmail.com>
2025-06-15 14:49:31 -0700

    Update main.rs

--- rust/Sessions/thread_1/src/main.rs
+++ rust/Sessions/thread_1/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let mut thread_handles = Vec::new();
 
     for i in 0..20 {
-        let handle = std::thread::spawn(move || {hello_thread(i); });  // closures are just inline functions, like when we do error.Go(func(){}), because in spawn, it takes a function that takes only one argument, so we use move to take ownership of i
+        let handle = std::thread::spawn(move || {hello_thread(i); });  // closures are just inline functions, like when we do error.Go(func(){}), because in spawn, it takes a function that takes no argument, so we use move to take ownership of i
         // we move the data, because rust memory manager wants thread to own the data
         thread_handles.push(handle);
     }
